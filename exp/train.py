114855de26528ebfba0185605c9e84b1bf857c92
arvind <arvind.m.vepa@gmail.com>
2019-03-09 16:00:10 -0800

    add feature code

--- exp/train.py
+++ exp/train.py
@@ -13,10 +13,11 @@ from sklearn.model_selection import KFold
 warnings.filterwarnings("ignore")
 
 
-def train_model(X, X_test, y, n_fold=10, params=None, model_type='lgb', plot_feature_importance=False, model=None):
+def train_model(X, y, X_test=None, n_fold=10, params=None, model_type='lgb', plot_feature_importance=False, model=None):
     """Taken from the `Earthquakes FE. More features and samples` kaggle notebook"""
     oof = np.zeros(len(X))
-    prediction = np.zeros(len(X_test))
+    if X_test is not None:
+        prediction = np.zeros(len(X_test))
     scores = []
     feature_importance = pd.DataFrame()
     folds = KFold(n_splits=n_fold, shuffle=True)
@@ -32,7 +33,9 @@ def train_model(X, X_test, y, n_fold=10, params=None, model_type='lgb', plot_fea
                       verbose=10000, early_stopping_rounds=200)
 
             y_pred_valid = model.predict(X_valid)
-            y_pred = model.predict(X_test, num_iteration=model.best_iteration_)
+
+            if X_test is not None:
+                y_pred = model.predict(X_test, num_iteration=model.best_iteration_)
 
         if model_type == 'xgb':
             train_data = xgb.DMatrix(data=X_train, label=y_train, feature_names=X.columns)
@@ -43,7 +46,8 @@ def train_model(X, X_test, y, n_fold=10, params=None, model_type='lgb', plot_fea
                               verbose_eval=500, params=params)
             y_pred_valid = model.predict(xgb.DMatrix(X_valid, feature_names=X.columns),
                                          ntree_limit=model.best_ntree_limit)
-            y_pred = model.predict(xgb.DMatrix(X_test, feature_names=X.columns), ntree_limit=model.best_ntree_limit)
+            if X_test is not None:
+                y_pred = model.predict(xgb.DMatrix(X_test, feature_names=X.columns), ntree_limit=model.best_ntree_limit)
 
         if model_type == 'sklearn':
             model = model
@@ -53,22 +57,19 @@ def train_model(X, X_test, y, n_fold=10, params=None, model_type='lgb', plot_fea
             score = mean_absolute_error(y_valid, y_pred_valid)
             print(f'Fold {fold_n}. MAE: {score:.4f}.')
             print('')
-
-            y_pred = model.predict(X_test).reshape(-1, )
-
+            if X_test is not None:
+                y_pred = model.predict(X_test).reshape(-1, )
         if model_type == 'cat':
             model = CatBoostRegressor(iterations=20000, eval_metric='MAE', **params)
             model.fit(X_train, y_train, eval_set=(X_valid, y_valid), cat_features=[], use_best_model=True,
                       verbose=False)
-
             y_pred_valid = model.predict(X_valid)
-            y_pred = model.predict(X_test)
-
+            if X_test is not None:
+                y_pred = model.predict(X_test)
         oof[valid_index] = y_pred_valid.reshape(-1, )
         scores.append(mean_absolute_error(y_valid, y_pred_valid))
-
-        prediction += y_pred
-
+        if X_test is not None:
+            prediction += y_pred
         if model_type == 'lgb':
             # feature importance
             fold_importance = pd.DataFrame()
@@ -76,8 +77,8 @@ def train_model(X, X_test, y, n_fold=10, params=None, model_type='lgb', plot_fea
             fold_importance["importance"] = model.feature_importances_
             fold_importance["fold"] = fold_n + 1
             feature_importance = pd.concat([feature_importance, fold_importance], axis=0)
-
-    prediction /= n_fold
+    if X_test is not None:
+        prediction /= n_fold
 
     print('CV mean score: {0:.4f}, mad: {1:.4f}.'.format(np.mean(scores), robust.mad(scores)))
 
@@ -92,5 +93,7 @@ def train_model(X, X_test, y, n_fold=10, params=None, model_type='lgb', plot_fea
             plt.figure(figsize=(16, 12));
             sns.barplot(x="importance", y="feature", data=best_features.sort_values(by="importance", ascending=False));
             plt.title('LGB Features (avg over folds)');
-
-    return np.mean(scores), robust.mad(scores)
+    if X_test is not None:
+        return np.mean(scores), robust.mad(scores), prediction
+    else:
+        return np.mean(scores), robust.mad(scores)
